9bd09d9685b42c4cecccdfcdde6f733ac9da0d3b
name <you@gmail.com>
2020-09-26 22:52:58 +0900

    完成品

---
+++ Assets/Sprict/GM.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GM : MonoBehaviour {
+    bool playF;
+     float speed;
+    float time = 10;
+
+    public GameObject GirlOb;
+    public Sprite Girl02;
+
+    public Text logT;
+    public GameObject button;
+
+    // Start is called before the first frame update
+    void Start() {
+        int levelF = Title.GetLevelF();
+        StartCoroutine("StartCo");
+        switch (levelF) {
+            case 0:
+            speed = 0.4f;
+            break;
+            case 1:
+            speed = 0.55f;
+            break;
+            case 2:
+            speed = 0.7f;
+            break;
+            default:
+            break;
+        }
+    }
+
+    IEnumerator StartCo() {
+        SpriteRenderer GirlRen = GirlOb.GetComponent<SpriteRenderer>();
+        logT.text = "よーい";
+        yield return new WaitForSeconds(1.5f);
+        logT.text = "はじめ !";
+        yield return new WaitForSeconds(1f);
+        GirlRen.sprite = Girl02;
+        playF = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (playF) {
+            time -= 1 * Time.deltaTime;
+            logT.text = ((int)time).ToString();
+
+            Vector2 pos = GirlOb.transform.position;
+            pos.x -= speed * Time.deltaTime;
+            if (Input.GetMouseButtonDown(0)) {
+                pos.x += 0.2f;
+            }
+            GirlOb.transform.position = pos;
+            if (pos.x >= 3.2f) {
+                logT.text = "勝ち !";
+                playF = false;
+                button.SetActive(true);
+            } else if (pos.x <= -3.2f) {
+                logT.text = "負け !";
+                playF = false;
+                button.SetActive(true);
+            } else if (time < 1) {
+                logT.text = "引き分け";
+                playF = false;
+                button.SetActive(true);
+            }
+        }
+    }
+
+  public   void RetryB() {
+        SceneManager.LoadScene("Game");
+    }
+    public void TitleB() {
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ Assets/Sprict/Title.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Title : MonoBehaviour
+{
+    public static  int levelF;
+    public GameObject Tuto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartB() {
+        Tuto.SetActive(true);
+    }
+
+    public void EasyB() {
+        levelF = 0;
+        Invoke("SceneGame",0.5f);
+    }
+    public void NormalB() {
+        levelF = 1;
+        Invoke("SceneGame", 0.5f);
+    }
+    public void HardB() {
+        levelF = 2;
+        Invoke("SceneGame", 0.5f);
+    }
+
+    void SceneGame() {
+        SceneManager.LoadScene ("Game");
+    }
+    public static int GetLevelF() {
+        return levelF;
+    }
+}
